ec73c33d350ba04956ef7e8f7f8fe27bb6a838e8
adeola <adeola@adeolas-MBP.fios-router.home>
2021-01-27 23:00:06 -0500

    initialized memory board

---
+++ app.js
@@ -0,0 +1,70 @@
+document.addEventListener("DOMContentLoaded", () => {
+	//card options with relative path
+	const cardArray = [
+		{
+			name: "download",
+			img: "assets/download.png",
+		},
+		{
+			name: "download",
+			img: "assets/download.png",
+		},
+		{
+			name: "email",
+			img: "assets/email.png",
+		},
+		{
+			name: "email",
+			img: "assets/download.png",
+		},
+		{
+			name: "games",
+			img: "assets/games.png",
+		},
+		{
+			name: "games",
+			img: "assets/games.png",
+		},
+		{
+			name: "git",
+			img: "assets/git.png",
+		},
+		{
+			name: "git",
+			img: "assets/git.png",
+		},
+		{
+			name: "music",
+			img: "assets/music.png",
+		},
+		{
+			name: "music",
+			img: "assets/music.png",
+		},
+		{
+			name: "network",
+			img: "assets/network.png",
+		},
+		{
+			name: "network",
+			img: "assets/network.png",
+		},
+	];
+
+	const grid = document.querySelector(".grid");
+	let cardsChosen = [];
+	let cardsChosenId = [];
+
+	//create board
+	function createBoard() {
+		for (let i = 0; i < cardArray.length; i++) {
+			let card = document.createElement("img");
+			card.setAttribute("src", "assets/brush.png");
+			card.setAttribute("data-id", i);
+			// card.addEventListener("click", flipcard)
+			grid.appendChild(card);
+		}
+	}
+
+	createBoard();
+});
